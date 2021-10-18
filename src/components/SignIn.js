61d8f0eb9119822b3ae67ce79701179ee62fbcb2
DevJessParker <jess.k.dempsey@gmail.com>
2021-10-18 15:18:20 -0700

    Auth modal created, toggle sign-in/sign-up tested with valid response object returned. Updated Login Context

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,48 +1,57 @@
-import React, { useContext } from "react";
-import { Form, Button } from "react-bootstrap";
-import axios from "axios";
-import base64 from "base-64";
+import React, { useContext, useState } from "react";
+import { Form, Button, Modal } from "react-bootstrap";
 import { LoginContext } from "../context/LoginContext";
 
 function SignIn() {
   const loginContext = useContext(LoginContext);
 
-  async function handleFormSubmit(e) {
+  const [showSignUpFields, setShowSignUpFields] = useState(true);
+
+  const handleToggle = () => loginContext.setShow(!loginContext.show);
+  
+  const handleShowSignUpFields = () => setShowSignUpFields(!showSignUpFields);
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    let user = {
-      username: e.target.formBasicText.value,
-      password: e.target.formBasicPassword.value,
-    };
-    console.log(user);
-    let auth = base64.encode(`${user.username}:${user.password}`);
-    const config = {
-      headers: { authorization: `Basic ${auth}` },
-      url: `https://squadfinderapp.herokuapp.com/signin`,
-      method: "post",
-    };
-    let results = await axios(config);
-    loginContext.setUser(results.data);
-    loginContext.setIsLoggedIn(true);
-    console.log(results.data);
+    showSignUpFields ? loginContext.signIn(e) : loginContext.signUp(e);
+    handleToggle();
   }
+
 
   return (
     <div>
-      <Form onSubmit={(e) => handleFormSubmit(e)}>
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" />
-          <Form.Text className="text-muted"></Form.Text>
+       <Button variant="primary" onClick={handleToggle}>
+        BUTTON TO LAUNCH SIGN-IN MODAL
+      </Button>
+      <Modal show={loginContext.show} onHide={handleToggle}>
+      <Modal.Header closeButton>
+          <Modal.Title>Sign-In to Squadfinder</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={(e) => handleFormSubmit(e)}>
+          <span onClick={handleShowSignUpFields}>{showSignUpFields ? 'Not a member? Click here to sign-up.' : 'Already a member? Click here to sign-in.'}</span>
+          <Form.Group className="mb-3" controlId="formBasicText">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Username" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail" hidden={showSignUpFields}>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Sign in
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {showSignUpFields ? 'Sign-In' : 'Sign-Up'}
+          </Button>
+        </Form>
+      </Modal>
     </div>
   );
 }
