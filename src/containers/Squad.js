1a5d644523817d79fff3008e5b34712251cdebe8
Twin-2 <davidwhit818@gmail.com>
2021-10-20 15:16:41 -0700

    finished squad page

--- src/containers/Squad.js
+++ src/containers/Squad.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Accordion } from "react-bootstrap";
+import axios from "axios";
+
+import { LoginContext } from "../context/LoginContext";
+
+const dummySquads = [
+  { name: "Squad Name", members: ["David", "Jaya", "Jess"] },
+  { name: "New Squad", members: ["Gina", "Jaya", "Jess"] },
+];
 
 function Squad() {
+  const [squads, setSquads] = useState([]);
+
+  const loginContext = useContext(LoginContext);
+
+  useEffect(() => {
+    async function getSquads() {
+      const config = {
+        method: "get",
+        url: `https://squadfinderapp.herokuapp.com/squads`,
+        headers: { authorization: `Bearer ${loginContext.user.token}` },
+      };
+      let results = await axios(config);
+      console.log(results.data);
+      setSquads(results.data);
+    }
+    getSquads();
+  }, []);
+
   return (
-    <div>
+    <div className="squadPage">
       <Link to="/createSquad">
-        <Button>Create Squad</Button>
+        <Button variant="green">Create Squad</Button>
       </Link>
-      Hello World
+      <Accordion>
+        {dummySquads.map((squad, idx) => (
+          <Accordion.Item eventKey={idx}>
+            <Accordion.Header>{squad.name}</Accordion.Header>
+            <Accordion.Body>
+              {squad.members.map((member) => (
+                <p>{member} </p>
+              ))}
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     </div>
   );
 }
